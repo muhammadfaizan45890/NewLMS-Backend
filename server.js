deab8692b5983aa080e6fd6ff43d8d393efd882b
Muhammad Faizan <muhammadfaizan45890@gmail.com>
2026-06-28 18:30:53 +0500

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 5000;
 // ------------------------------------------------------------
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://lms-courseacademy.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
